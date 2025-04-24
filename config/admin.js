cb2ec4baf7e1f5d9f955e1b72f6998915f0e63f6
yw738 <https://github.com/yw738>
2025-04-24 19:03:12 +0800

    配置优化

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: "lWGXPcbZqULCmFiZyouvxw==",
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "f4YftW28WeItktStfHZ16A==",
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: "f4YftW28WeItktStfHZ16A==",
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
